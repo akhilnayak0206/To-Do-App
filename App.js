8ea3cb89f3284d1a28401e44e4984a9d05caa8c2
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-01-20 14:36:06 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Make changes in App.js</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
